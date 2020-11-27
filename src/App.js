fa68d7cf548228587f870588756f1c6a9e6ca970
habaumugisha1 <habajeunes2@gmail.com>
2020-11-27 15:36:00 +0200

    new react weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react'
+import Weather from './component/weather.component'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './component/form.component';
+const API_key = 'ef476b209998f6874e729c4400b0e08b'
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      city:undefined,
+      country:undefined,
+      main: undefined,
+      celcius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description:"",
+      icon: undefined,
+      error: false
+    };
+    // this.getWeather();
+    // this.weatherIcon={
+    //   Thunderstorm:"wi-thunderstorm"
+    // }
+  }
+  calCelsius(temp){
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+  getWeather = async (event) => {
+    event.preventDefault()
+    const city = event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+    if(city && country){
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`);
+
+    const response = await api_call.json();
+    console.log(response);
+    this.setState({
+      city: `${response.name}, ${response.sys.country}`,
+      celcius:response.main.celcius,
+      celsius:this.calCelsius(response.main.temp),
+      temp_min:this.calCelsius(response.main.temp_min),
+      temp_max:this.calCelsius(response.main.temp_max),
+      description:response.weather[0].description,
+      icon:response.weather[0].icon
+    });
+    } else{
+      this.setState({error:true});
+    }
+
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < Form loadweather={this.getWeather} error={this.state.error}/>
+      < Weather city={this.state.city} country={this.state.country} temp_celsius={this.state.celsius} temp_max={this.state.temp_max} temp_min={this.state.temp_min} description={this.state.description}
+      icon={this.state.icon}
+      />
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/component/weather.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './form.style.css'
+const Weather = (props) => {
+    return(
+        <div className="container">
+            <div className="cards pt-4">
+            <h1>{props.city}</h1>
+            {icons(props.icon)}
+            {props.temp_celsius?(<h1 className="py2">{props.temp_celsius}&deg;</h1>):null}
+            
+                {minmaxTemp(props.temp_min, props.temp_max)}
+                <h4 className="py-4">{props.description}</h4>
+            </div>
+        </div>
+    );
+};
+function icons(icon){
+    if(icon){
+        return(
+            <h5 className="py-4">
+        <img className="img-fluid" src={`https://openweathermap.org/img/w/${icon}.png`} alt="icon" />
+           {/* <i className={`${this.props.weatherIcon}`}></i> */}
+        </h5>
+        )
+    }
+}
+function minmaxTemp(min, max){
+    if(min && max) {
+        return(
+            <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+            </h3>
+        )
+    }
+}
+export default Weather
